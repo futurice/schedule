d5e71f5801404a9de59ab586ae68c2ac06bf4f5c
Mihai <mbor@futu>
2014-05-13 16:57:56 +0300

    create calendar events; add a test which creates&deletes an event

--- introsite/futuintro/tests.py
+++ introsite/futuintro/tests.py
@@ -1,38 +1,46 @@
-from django.test import TestCase
-from futuintro import util
+import datetime
+import dateutil.parser
 import json
+import pytz
+from django.test import TestCase
+from futuintro import calendar, util, testutil
 
 class DummyTest(TestCase):
 
-    def test_dummy(self):
-        calSvc = util.buildCalendarSvc()
+    def test_create_and_delete_event(self):
+        calId = calendar.futuintroCalId
+        tzName = calendar.berlinTz
 
-        event = {
-                'summary': 'A Test event',
-                'location': 'Kitchen',
-                'start': {
-                    'dateTime': '2014-05-13T09:00:00.000+03:00',
-                },
-                'end': {
-                    'dateTime': '2014-05-13T09:30:00.000+03:00',
-                },
-                'attendees': [
-                    {
-                        'email': 'user.name@futurice.com',
-                    },
-                ],
-        }
+        startDt = datetime.datetime.today() + datetime.timedelta(days=1)
+        startDt = datetime.datetime(startDt.year, startDt.month, startDt.day,
+                9, 45)
+        endDt = startDt + datetime.timedelta(minutes=30)
+        attendingEmails = [testutil.Alice_email, testutil.Brad_email]
+        ev = calendar.createEvent(calId, False, 'TEST: sample event',
+                'Test: create & delete a simple event', 'Reception',
+                startDt, endDt, tzName, attendingEmails)
 
-        # TODO: make a "Futurice Onboardings" Calendar and use its ID here,
-        # we're now putting this on the developer's own calendar when he/she
-        # runs this test.
-        createdEvent = calSvc.events().insert(
-                calendarId='primary', body=event).execute()
-        evId = createdEvent['id']
+        def parseToTimezone(string, tz):
+            """Parse a datetime string and convert it to timezone tz."""
+            return dateutil.parser.parse(string).astimezone(tz)
 
-        retrievedEvent = calSvc.events().get(
-                calendarId='primary', eventId=evId).execute()
-        print(json.dumps(retrievedEvent, indent=2))
+        def sameLocalFields(dt1, dt2):
+            """Compare year, month, day, hour, minute and second in args."""
+            return (dt1.year == dt2.year and dt1.month == dt2.month
+                    and dt1.day == dt2.day and dt1.hour == dt2.hour
+                    and dt1.minute == dt2.minute and dt1.second == dt2.second)
 
-        # TODO: check something useful, e.g. the attendees' emails
-        self.assertEqual(retrievedEvent['id'], createdEvent['id'])
+        print(json.dumps(ev, indent=2))
+        # Google returns an ISO dateTime string which includes a UTC offset
+        # (apparently the local offset for you). Convert to local time for the
+        # timezone we requested above, and check the date&time.
+        tzObj = pytz.timezone(tzName)
+        self.assertTrue(sameLocalFields(startDt,
+            parseToTimezone(ev['start']['dateTime'], tzObj)))
+        self.assertTrue(sameLocalFields(endDt,
+            parseToTimezone(ev['end']['dateTime'], tzObj)))
+
+        self.assertTrue({x for x in attendingEmails}
+                == {x['email'] for x in ev['attendees']})
+
+        #calendar.deleteEvent(calId, ev['id'])
---
+++ introsite/futuintro/testutil.py
@@ -0,0 +1,3 @@
+# Test User emails
+Alice_email = 'alice.onboarding@futurice.com'
+Brad_email = 'brad.onboarding@futurice.com'
