3171396779f7e687e8974d6290be1eb431ebdc99
Mihai <mbor@futu>
2014-05-14 16:39:15 +0300

    start extending the django User model

--- introsite/introsite/settings.py
+++ introsite/introsite/settings.py
@@ -44,8 +44,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.RemoteUserBackend',
 )
 
 ROOT_URLCONF = 'introsite.urls'
