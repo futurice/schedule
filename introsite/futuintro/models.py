d4864fce4f4c34db937d11021c0aa6c87528c4a5
Mihai <mbor@futu>
2014-05-15 14:56:19 +0300

    data model: first iteration. This will change.

--- introsite/futuintro/models.py
+++ introsite/futuintro/models.py
@@ -10,3 +10,40 @@ class FutuUser(models.Model):
     def __unicode__(self):
         # not ideal, does a DB lookup
         return unicode(self.user.username)
+
+
+class ScheduledEventGroup(models.Model):
+    """
+    A set of ScheduledEvents which were created together.
+
+    E.g. they represent the onboarding program for one employee.
+    """
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+class ScheduledEvent(models.Model):
+    """
+    An event which exists in Google Calendar.
+
+    Has 'Scheduled' in the name so we don't confuse it with an event template.
+    """
+    jsonData = models.BinaryField()
+    eventGroup = models.ForeignKey(ScheduledEventGroup)
+
+
+class EventTemplate(models.Model):
+    summary = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    # which calendar day the event is on. 1 is the employee's starting day.
+    dayNumber = models.PositiveSmallIntegerField()
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    timezone = models.CharField(max_length=100)
+
+    inviteEmployee = models.BooleanField()
+    inviteEmployee.default = True
+
+    inviteSupervisor = models.BooleanField()
+    inviteSupervisor.default = False
+    otherInvitees = models.ManyToManyField(User)
