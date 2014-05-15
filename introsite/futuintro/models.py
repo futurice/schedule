464324284b39ae7b0abad1c826eb5db62ef81b32
Mihai <mbor@futu>
2014-05-15 11:11:03 +0300

    add FutuUser model for extra user data (currently just supervisor)

--- introsite/futuintro/models.py
+++ introsite/futuintro/models.py
@@ -6,3 +6,7 @@ class FutuUser(models.Model):
     user = models.OneToOneField(User)
     supervisor = models.ForeignKey(User, related_name='supervisor_of',
             null=True)
+
+    def __unicode__(self):
+        # not ideal, does a DB lookup
+        return unicode(self.user.username)
--- introsite/futuintro/util.py
+++ introsite/futuintro/util.py
@@ -3,7 +3,11 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 
+from django.contrib.auth.models import User
+from futuintro.models import FutuUser
+
 import httplib2
+import json
 
 
 def ensureOAuthCredentials():
@@ -37,3 +41,30 @@ def buildCalendarSvc():
     http = credentials.authorize(http)
     return build(serviceName='calendar', version='v3', http=http,
             developerKey='YOUR_DEVELOPER_KEY')
+
+def createUsers(jsonDumpFile):
+    """
+    Create Users from a .json file dumped from FUM (see README).
+
+    This is a helper for DEV.
+    The users we create will have different IDs than the json dump.
+    """
+    with open(jsonDumpFile, 'r') as f:
+        dump = json.load(f)
+    # de-duplicate first (the dump has a duplicate)
+    userById = {u['id']: u for u in dump}
+    for u in userById.values():
+        userById[u['id']] = u
+        newUser = User.objects.create_user(u['username'], u['email'])
+        if u['first_name']:
+            newUser.first_name = u['first_name']
+        if u['last_name']:
+            newUser.last_name = u['last_name']
+        newUser.save()
+    for u in userById.values():
+        fu = FutuUser()
+        fu.user = User.objects.get(username=u['username'])
+        if u['supervisor']:
+            fu.supervisor = User.objects.get(
+                    username=userById[u['supervisor']]['username'])
+        fu.save()
