1c276462c3df3e6bd4ce6b96a6532a60ba392edc
Mihai <mbor@futu>
2014-07-09 12:19:13 +0300

    fix wsgi.py for improved Django settings.py in previous commit

--- schedulesite/schedulesite/wsgi.py
+++ schedulesite/schedulesite/wsgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schedulesite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schedulesite.settings")
 
 from django.core.wsgi import get_wsgi_application
-_application = get_wsgi_application()
-
-def application(environ, start_response):
-    os.environ['DJANGO_SETTINGS_MODULE'] = environ['DJANGO_SETTINGS_MODULE']
-    return _application(environ, start_response)
+application = get_wsgi_application()
