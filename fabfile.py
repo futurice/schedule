d55137d4c4a89ab35f1b7f1e69833c84e0814438
Mihai <mihaiftr@users.noreply.github.com>
2015-01-09 08:29:34 +0200

    improve deploy: isolated code and venv dir for each deployment

--- fabfile.py
+++ fabfile.py
@@ -1,31 +1,90 @@
+import datetime
 from fabric.api import task, env
 from fabric.context_managers import cd, prefix, settings
 from fabric.operations import sudo
+import os, os.path
+import random
+import string
+
 
 env.hosts = env.hosts or ['artunium.futurice.com']
-code_dir = '/home/futuschedule/futuschedule'
-code_dir_user = 'futuschedule'
-venv_dir = '/home/futuschedule/venv'
+schedule_user = 'futuschedule'
+home_dir = '/home/futuschedule'
+
+stamp = datetime.datetime.utcnow().isoformat().replace(':', '').replace('-', '')
+stamp = stamp[:stamp.rfind('.')] + 'Z'
+config_dir = os.path.join(home_dir, 'config-files')
+repo_link = os.path.join(home_dir, 'futuschedule')
+env_link = os.path.join(home_dir, 'venv')
+repo_dir = os.path.join(home_dir, 'futuschedule-' + stamp)
+env_dir = os.path.join(home_dir, 'venv-' + stamp)
+manage_py = os.path.join(repo_dir, 'schedulesite', 'manage.py')
+
+git_clone_url = 'https://github.com/futurice/schedule.git'
+
 
 @task
 def stop_services():
     sudo('supervisorctl stop task-processor')
     sudo('service apache2 stop')
 
+
 @task
 def start_services():
     sudo('supervisorctl start task-processor')
     sudo('service apache2 start')
 
-def updateCode():
-    with settings(cd(code_dir), sudo_user=code_dir_user):
-        sudo('git pull')
-        with prefix('source ' + venv_dir + '/bin/activate'):
-            sudo('pip install -r req.txt')
-            sudo('./schedulesite/manage.py migrate')
+
+def clone_repository():
+    with settings(sudo_user=schedule_user):
+        sudo('git clone ' + git_clone_url + ' ' + repo_dir)
+
+
+def make_env():
+    with settings(sudo_user=schedule_user):
+        sudo('virtualenv ' + env_dir)
+        with prefix('source ' + os.path.join(env_dir, 'bin/activate')):
+            sudo('pip install -r ' + os.path.join(repo_dir, 'req.txt'))
+
+
+def prepare_repository():
+    with settings(sudo_user=schedule_user):
+        settings_dir = os.path.join(repo_dir, 'schedulesite', 'schedulesite')
+        with settings(cd(settings_dir)):
+            sudo('cp settings_prod.py.template settings.py')
+            chars = string.ascii_letters + string.digits
+            secret_key = ''.join(random.choice(chars) for x in range(40))
+            sudo('echo \'SECRET_KEY = "' + secret_key + '"\' >>settings.py')
+
+
+def run_tests():
+    with settings(cd(config_dir), sudo_user=schedule_user):
+        with prefix('source ' + os.path.join(env_dir, 'bin/activate')):
+            sudo(manage_py + ' test futuschedule')
+
+
+def migrate():
+    with settings(sudo_user=schedule_user):
+        with prefix('source ' + os.path.join(env_dir, 'bin/activate')):
+            sudo(manage_py + ' migrate')
+
+
+def move_symlinks():
+    with settings(sudo_user=schedule_user):
+        sudo('rm -f ' + env_link + ' ' + repo_link)
+        sudo('ln -s ' + env_dir + ' ' + env_link)
+        sudo('ln -s ' + repo_dir + ' ' + repo_link)
+
 
 @task
 def deploy():
     stop_services()
-    updateCode()
+    clone_repository()
+    make_env()
+    prepare_repository()
+    run_tests()
+    migrate()
+    move_symlinks()
     start_services()
+
+    print('Completed deploy with timestamp {}'.format(stamp))
