4f8aca3995e240e7591b2f168f492f2fa357031d
Jussi Vaihia <jussiava@gmail.com>
2018-02-26 13:33:58 +0200

    USERS_URL for users

--- schedulesite/futuschedule/management/commands/refresh_users.py
+++ schedulesite/futuschedule/management/commands/refresh_users.py
@@ -2,59 +2,52 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-import slumber
-import _slumber_auth
+import json
+import requests
 
 class Command(BaseCommand):
-    help = 'Fetch users from FUM, adding new, updating old and removing removed users from the database'
+    help = 'Fetch users, add new, update old and remove gone users from the database'
+
+    def get_users(self):
+        data = requests.get(os.getenv('USERS_URL'))
+        users = json.loads(data)
+        return users
+
+    def get_email(self, user):
+        return '%s@futurice.com'%(user['username'])
 
     def handle(self, *args, **options):
-        api = slumber.API(settings.FUM_API_URL, auth=_slumber_auth.TokenAuth(settings.FUM_API_TOKEN))
+        users = self.get_users()
 
-        pageIndex = 1
-        users = []
-        while(True):
-            data = api.users().get(page=pageIndex, fields='username, first_name, last_name, email, google_status, status, supervisor, id')
-
-            #filter inactive users and users with missing data
-            usersData = filter(lambda u: u['username'] and u['email'] and
-                u['first_name'] and u['last_name'] and
-                u['google_status'] == 'activeperson' and u['status'] == 'active', data["results"])
-
-            users = users + usersData
-            if ("next" not in data or data["next"] == None):
-                break               
-            pageIndex += 1
-
-        #the following code is copied from the old implementation of the same task
         UM = get_user_model()
-        userById = {u['id']: u for u in users}
 
         #remove users not in the user list from FUM
-        keepUsernames = {u['username'] for u in users}
-        for oldUser in UM.objects.all():
-            if oldUser.username not in keepUsernames:
-                oldUser.delete()
+        usersList = [u['username'] for u in users]
+        oldUsers = UM.objects.all().values_list('username', flat=True)
+        for username in oldUsers:
+            if username not in usersList:
+                UM.objects.filter(username=username).delete()
 
         #update existing users and add new ones
-        for u in userById.values():
+        for u in users:
             try:
                 newUser = UM.objects.get(username=u['username'])
-                newUser.email = u['email']
-                newUser.first_name = u['first_name']
-                newUser.last_name = u['last_name']
+                newUser.email = self.get_email(u)
+                newUser.first_name = u['first']
+                newUser.last_name = u['last']
                 newUser.save()
             except UM.DoesNotExist as e:
-                newUser = UM.objects.create_user(u['username'], u['email'],
-                    u['first_name'], u['last_name'])
+                newUser = UM.objects.create_user(u['username'],
+                            self.get_email(u),
+                            u['first'],
+                            u['last'])
         
         #save supervisors for users
-        for u in userById.values():
-            if u['supervisor']:
+        for u in users:
+            if u.get('supervisor', None):
                 a = UM.objects.get(username=u['username'])
                 try:
-                    a.supervisor = UM.objects.get(
-                        username=userById[u['supervisor']]['username'])
+                    a.supervisor = UM.objects.get(username=u['supervisor'])
                 except Exception as e:
                     print(e)
                 a.save()
