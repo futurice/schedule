3a6bf7d214b672942d2f9e74c2870845b1f309ff
Jussi Vaihia <jussiava@gmail.com>
2018-05-24 11:52:14 +0200

    allow asset_tag failure while developing

--- schedulesite/schedulesite/settings.py
+++ schedulesite/schedulesite/settings.py
@@ -81,7 +81,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-asset_tag = lambda: open('/opt/tag').read()
+def asset_tag():
+    try:
+        return open('/opt/tag').read()
+    except Exception as e:
+        print(e)
 STATIC_URL = '/static/%s/'%(asset_tag())
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
